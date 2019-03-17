0c906e8f9bce006f721266e262f1766b18f8adfc
Do Tri Nam <t8dotr00@students.oamk.fi>
2019-03-17 12:24:28 +0200

    Fixed bug: showing Incorrect for every answer

--- src/components/gameplay/gameplay.jsx
+++ src/components/gameplay/gameplay.jsx
@@ -1,38 +1,79 @@
 import React, { Component } from "react";
 
-class Gameplay extends Component {
+export default class Gameplay extends Component {
   state = {
-    result:{
-      you:0,
-      opponent:0,
+    result: {
+      you: 0,
+      opponent: 0
     },
     rounds: this.props.currentMatch.matchData.rounds,
     currentRound: 0,
-    currentQ: 0,
+    currentQ: 0
   };
   validateAnswer = e => {
-    alert(e.target.textContent == this.state.rounds[this.state.currentRound].questions[this.state.currentQ].answer ? 'Correct' : "Incorrect");
+    console.log(
+      e.currentTarget.textContent.trim().localeCompare(
+          this.state.rounds[this.state.currentRound].questions[this.state.currentQ].answer
+        ) === 0
+        ? "Correct"
+        : "Incorrect"
+    );
     let currentQ = this.state.currentQ;
     currentQ++;
-    this.setState({currentQ});
-  }
+    if (
+      currentQ < this.state.rounds[this.state.currentRound].questions.length
+    ) {
+      this.setState({ currentQ: currentQ });
+    } else {
+      let currentRound = this.state.currentRound;
+      currentRound++;
+      if (currentRound < this.state.rounds.length) {
+        this.setState({ currentRound: currentRound });
+        this.setState({ currentQ: 0 });
+      } else {
+        this.props.changeScreen(1);
+      }
+    }
+  };
   buttonRenderer = idButton => {
-    return this.state.rounds[this.state.currentRound].questions[this.state.currentQ].options[idButton];
-  }
+    return this.state.rounds[this.state.currentRound].questions[
+      this.state.currentQ
+    ].options[idButton];
+  };
   render() {
     return (
-
       <React.Fragment>
-        <img alt={this.state.rounds[this.state.currentRound].topic + " icon"} id="categoryIcon"/>
-        <div id="gameQuestion" >{this.state.rounds[this.state.currentRound].questions[this.state.currentQ].question}</div>
-        <button className="gameOptions" onClick={this.validateAnswer}> {this.buttonRenderer(0)} </button>
-        <button className="gameOptions" onClick={this.validateAnswer}> {this.buttonRenderer(1)} </button><br></br>
-        <button className="gameOptions" onClick={this.validateAnswer}> {this.buttonRenderer(2)} </button>
-        <button className="gameOptions" onClick={this.validateAnswer}> {this.buttonRenderer(3)} </button><br></br>
-        <button onClick={()=>this.props.changeScreen(0)}>Change</button>
+        <img
+          alt={this.state.rounds[this.state.currentRound].topic + " icon"}
+          id="categoryIcon"
+        />
+        <div id="gameQuestion">
+          {
+            this.state.rounds[this.state.currentRound].questions[
+              this.state.currentQ
+            ].question
+          }
+        </div>
+        <button className="gameOptions" onClick={this.validateAnswer}>
+          {" "}
+          {this.buttonRenderer(0)}{" "}
+        </button>
+        <button className="gameOptions" onClick={this.validateAnswer}>
+          {" "}
+          {this.buttonRenderer(1)}{" "}
+        </button>
+        <br />
+        <button className="gameOptions" onClick={this.validateAnswer}>
+          {" "}
+          {this.buttonRenderer(2)}{" "}
+        </button>
+        <button className="gameOptions" onClick={this.validateAnswer}>
+          {" "}
+          {this.buttonRenderer(3)}{" "}
+        </button>
+        <br />
+        <button onClick={() => this.props.changeScreen(0)}>Change</button>
       </React.Fragment>
     );
   }
 }
-// look for how to change props of button component
-export default Gameplay;
